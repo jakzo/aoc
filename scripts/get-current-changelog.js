7c003833f43f11c75cacf5cc2043c54e446d7323
jakzo <jack@jf.id.au>
2020-12-28 12:40:44 +1100

    ci

--- scripts/get-current-changelog.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const fs = require('fs')
-const { getChangelogEntry } = require('@changesets/release-utils')
-
-const changelog = fs.readFileSync(path.join(__dirname, '..', 'CHANGELOG.md'), 'utf8')
-const version = require('../package.json').version
-console.log(getChangelogEntry(changelog, version).content)
